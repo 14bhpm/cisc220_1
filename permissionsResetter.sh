9a76a752488fbe8d2543902834cc0b05b91c8b73
Beltan Michel <14bhpm@queensu.ca>
2016-10-23 19:55:23 -0400

    Rewrote the files, solved mysterious $'\r' error.

--- permissionsResetter.sh
+++ permissionsResetter.sh
@@ -1,30 +1,29 @@
-# Rawan Alsaadi 10081442 
+# Rawan Alsaadi 10081442
 # Omar Toutounji 10169506
-# Beltan Michel 10169162
+# Beltan Micehl 10169162
 
 #!/bin/bash
 mode=$1
 fName=$2
 readarray -t permissionsArray < $fName
 
-
 if [ "$mode" == "-s" ]; then
-	 printf "%20s %20s %20s\n" "Old        Current      File";
+	printf "%20s %20s %20s\n" "Old         Current      File";
 fi
 
 for line in "${permissionsArray[@]}"; do
    savedPermission=${line%% *}
-   file=${line#* } 
+   file=${line#*}
 
    currentPermission="$(stat -c "%A" $file)"
    if [ "$currentPermission" != "$savedPermission" ]; then
-     if [ "$mode" == "-s" ]; then       
+     if [ "$mode" == "-s" ]; then
        printf "%20s %20s %20s\n" "$savedPermission $currentPermission $file"
-     elif [ "$mode"=="-r" ]; then
+     elif [ "$mode" == "-r" ]; then
        echo "$file Reset from $currentPermission to $savedPermission"
-      
-	  chmod a-rwx $file	
+
+          chmod a-rwx $file
        chmod u+${savedPermission:1:3},g+${savedPermission:4:3},o+${savedPermission:7:3} $file
-     fi
-   fi
+      fi
+    fi
 done
--- permissionsSaver.sh
+++ permissionsSaver.sh
@@ -1,17 +1,16 @@
-# Rawan Alsaadi 10081442 
+# Rawan Alsaadi 10081442
 # Omar Toutounji 10169506
 # Beltan Michel 10169162
-
 #!/bin/bash
+
 originalFile="savedPermissions"
 nextFileName=$originalFile
 
 j=1
 
-while [ -f "$nextFileName" ] 
-do  
-  nextFileName=$originalFile$j
-  j=$(($j + 1 ))
+while [ -f "$nextFileName" ]; do
+ nextFileName=$originalFile$j
+ j=$(($j + 1))
 done
 
 stat -c "%A %n" * >$nextFileName
